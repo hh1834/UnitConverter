b0fd8077a044b1dd39aff43ce0aa64fc0e7059dc
Hansol Hong <hh1834@nyu.edu>
2023-06-17 16:09:48 -0500

    Convert kilos to pounds on button click

--- app/src/main/java/com/project/unitconverter/MainActivity.java
+++ app/src/main/java/com/project/unitconverter/MainActivity.java
@@ -3,11 +3,14 @@ package com.project.unitconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static double KILOS_TO_POUNDS = 2.20462;
 
     TextView titleView;
     TextView resultView;
@@ -18,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputView = findViewById(R.id.editText);
+        button = findViewById(R.id.btn);
+        resultView = findViewById(R.id.resultView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double inputInKilos = Double.parseDouble(inputView.getText().toString());
+                resultView.setText(inputInKilos * KILOS_TO_POUNDS + " lb");
+            }
+        });
     }
 }
